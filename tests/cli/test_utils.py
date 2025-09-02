456160894ae82543bc0b08c59eb6df49da783449
Fabrice Normandin <normandf@mila.quebec>
2025-09-02 15:47:24 -0400

    Update test for hostname to use for computenode
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- tests/cli/test_utils.py
+++ tests/cli/test_utils.py
@@ -45,7 +45,8 @@ def test_hostname():
 @pytest.mark.parametrize(
     ("cluster_name", "node", "expected"),
     [
-        ("mila", "cn-a001", "cn-a001.server.mila.quebec"),
+        ("mila", "cn-a001", "cn-a001"),
+        ("mila", "cn-a001.server.mila.quebec", "cn-a001.server.mila.quebec"),
     ]
     + [
         # Host !beluga  bc????? bg????? bl?????
